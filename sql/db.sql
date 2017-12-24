7e9d1a0835b8062df8c316ac38502d0f7d2176e8
Dariusz Ustrzycki <*newsecond@outlook.com*>
2017-12-24 13:32:43 +0100

    Add Polish encoding to sql script

--- sql/db.sql
+++ sql/db.sql
@@ -1,31 +1,33 @@
 
-CREATE DATABASE school;
+CREATE DATABASE school 
+	CHARACTER SET utf8mb4 
+	COLLATE utf8_polish_ci;
 
 CREATE TABLE user_group( 
 	id INT AUTO_INCREMENT, 
-	name VARCHAR(255), 
+	name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8_polish_ci, 
 	PRIMARY KEY(id));
 
 CREATE TABLE user (
   	id BIGINT UNSIGNED AUTO_INCREMENT,
-  	username varchar(255) NOT NULL UNIQUE,
-  	email varchar(255) NOT NULL UNIQUE,
-  	password varchar(255) NOT NULL,
+  	username VARCHAR(255) NOT NULL UNIQUE CHARACTER SET utf8mb4 COLLATE utf8_polish_ci, 
+  	email VARCHAR(255) NOT NULL UNIQUE,
+  	password VARCHAR(255) NOT NULL,
   	user_group_id INT DEFAULT 1, -- by default everyone has the status of a student
   	PRIMARY KEY (id),
   	FOREIGN KEY (user_group_id) REFERENCES user_group (id) );
   
 CREATE TABLE exercise( 
   	id INT AUTO_INCREMENT, 
-  	title VARCHAR(255), 
-  	description TEXT, 
+  	title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8_polish_ci,  
+  	description TEXT CHARACTER SET utf8mb4 COLLATE utf8_polish_ci, 
   	PRIMARY KEY(id) );
   
 CREATE TABLE solution( 
 	id INT AUTO_INCREMENT, 
 	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
 	updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-	description TEXT, 
+	description TEXT CHARACTER SET utf8mb4 COLLATE utf8_polish_ci, 
 	exercise_id INT, 
 	user_id BIGINT, 
 	PRIMARY KEY(id), 
