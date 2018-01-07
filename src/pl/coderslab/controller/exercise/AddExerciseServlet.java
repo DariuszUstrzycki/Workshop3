ea5b4680651de767d287d09cae850deaabd975e8
Dariusz Ustrzycki <*newsecond@outlook.com*>
2018-01-07 20:57:05 +0100

    Adapt servlet and jsp so that exercise is added and displayed w/h userId

--- src/pl/coderslab/controller/exercise/AddExerciseServlet.java
+++ src/pl/coderslab/controller/exercise/AddExerciseServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import pl.coderslab.dao.ExerciseDao;
 import pl.coderslab.dao.MySQLExerciseDao;
@@ -28,17 +29,16 @@ public class AddExerciseServlet extends HttpServlet {
 		
 		String title = request.getParameter("title");
 		String description = request.getParameter("description");
-		String userIdString = request.getParameter("userId");
+		Long userId = getUserId(request);
 		
-		System.out.println("Entering post with " + title + ", " + description + ", " + userIdString);
+		System.out.println("Entering post with " + title + ", " + description + ", " + userId);
 		
-		if (nullOrEmpty(title, description, userIdString)) {
+		if (nullOrEmpty(title, description) || userId == null) {
 			forwardToFormPage(request, response, "Ooops, the title/description fields can't be empty!");
 			return;
 		} else {
 			ExerciseDao dao = new MySQLExerciseDao();
 			System.out.println("About to add exercise");
-			long userId = Long.parseLong(userIdString);
 			long id = dao.save(new Exercise(title, description, userId));
 			if (id > 0) {
 				System.out.println("id of exercise is " + id);
@@ -48,10 +48,21 @@ public class AddExerciseServlet extends HttpServlet {
 		}
 		
 	}
+
+	private Long getUserId(HttpServletRequest request) {
+		Long id = null;
+		HttpSession session = request.getSession(false);
+		if(session!= null) {
+			User user = (User)session.getAttribute("loggedUser");
+			if(user != null) {
+				id = user.getId();
+			}
+		}
+		return id;
+	}
 	
-	private boolean nullOrEmpty(String title, String description, String userId) {
-		return (title == null || title.equals("") || description == null || description.equals("") ||
-				userId == null || userId.equals(""));
+	private boolean nullOrEmpty(String title, String description) {
+		return (title == null || title.equals("") || description == null || description.equals(""));
 	}
 	
 	private void forwardToFormPage(HttpServletRequest request, HttpServletResponse response, String message)
